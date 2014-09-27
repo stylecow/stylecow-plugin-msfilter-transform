56938e3ce8441ae632824412044ec75d42c95784
oscarotero <oom@oscarotero.com>
2014-09-27 11:05:46 +0200

    updated

--- index.js
+++ index.js
@@ -1,99 +1,102 @@
 require('sylvester');
 
-module.exports = {
-	disable: {
-		explorer: 9.0
-	},
-	Declaration: {
-		transform: function (declaration) {
-			var rule = declaration.ancestor('Rule');
-			var matrix = [];
+module.exports = function (stylecow) {
 
-			declaration.search('Function').forEach(function (fn) {
-				var args = fn.content;
+	stylecow.addTask({
+		disable: {
+			explorer: 9.0
+		},
+		Declaration: {
+			transform: function (declaration) {
+				var rule = declaration.ancestor('Rule');
+				var matrix = [];
 
-				switch (fn.name) {
-					case "rotate":
-						if (args[0].indexOf('deg') !== -1) {
-							var deg = parseInt(args[0], 10);
+				declaration.search('Function').forEach(function (fn) {
+					var args = fn.content;
 
-							if (deg < 0) {
-								deg += 360;
+					switch (fn.name) {
+						case "rotate":
+							if (args[0].indexOf('deg') !== -1) {
+								var deg = parseInt(args[0], 10);
+
+								if (deg < 0) {
+									deg += 360;
+								}
+
+								switch (deg) {
+									case 90:
+										rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=1)');
+										break;
+
+									case 180:
+										rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=2)');
+										break;
+
+									case 270:
+										rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=3)');
+										break;
+
+									case 360:
+										rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=4)');
+										break;
+
+									default:
+										matrix.push(getMatrix(fn.name, args));
+								}
+							} else {
+								matrix.push(getMatrix(fn.name, args));
 							}
+							break;
 
-							switch (deg) {
-								case 90:
-									rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=1)');
-									break;
-
-								case 180:
-									rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=2)');
-									break;
-
-								case 270:
-									rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=3)');
-									break;
-
-								case 360:
-									rule.addOldMsFilter('progid:DXImageTransform.Microsoft.BasicImage(rotation=4)');
-									break;
-
-								default:
-									matrix.push(getMatrix(fn.name, args));
+						case "scaleX":
+							if (args[0] == -1) {
+								rule.addOldMsFilter('flipH');
+							} else {
+								matrix.push(getMatrix(fn.name, args));
 							}
-						} else {
-							matrix.push(getMatrix(fn.name, args));
-						}
-						break;
+							break;
 
-					case "scaleX":
-						if (args[0] == -1) {
-							rule.addOldMsFilter('flipH');
-						} else {
-							matrix.push(getMatrix(fn.name, args));
-						}
-						break;
+						case "scaleY":
+							if (args[0] == -1) {
+								rule.addOldMsFilter('flipV');
+							} else {
+								matrix.push(getMatrix(fn.name, args));
+							}
+							break;
+		
+						case "scale":
+							if (args[0] == -1 && args[1] == -1) {
+								rule.addOldMsFilter('flipH, flipV');
+							} else {
+								matrix.push(getMatrix(fn.name, args));
+							}
+							break;
 
-					case "scaleY":
-						if (args[0] == -1) {
-							rule.addOldMsFilter('flipV');
-						} else {
+						case "matrix":
+						case "skew":
+						case "skewX":
+						case "skewY":
 							matrix.push(getMatrix(fn.name, args));
+							break;
 						}
-						break;
-	
-					case "scale":
-						if (args[0] == -1 && args[1] == -1) {
-							rule.addOldMsFilter('flipH, flipV');
-						} else {
-							matrix.push(getMatrix(fn.name, args));
-						}
-						break;
+				});
 
-					case "matrix":
-					case "skew":
-					case "skewX":
-					case "skewY":
-						matrix.push(getMatrix(fn.name, args));
-						break;
+
+				if (matrix.length) {
+					var m = matrix[0];
+
+					for (var k = 0; k < matrix.length; k++) {
+						if (matrix[k+1]) {
+							m = m.x(matrix[k+1]);
+						}
 					}
-			});
 
-
-			if (matrix.length) {
-				var m = matrix[0];
-
-				for (var k = 0; k < matrix.length; k++) {
-					if (matrix[k+1]) {
-						m = m.x(matrix[k+1]);
-					}
+					rule.addOldMsFilter('progid:DXImageTransform.Microsoft.Matrix(sizingMethod="auto expand", M11 = ' + m.elements[0][0] + ', M12 = ' + m.elements[0][1] + ', M21 = ' + m.elements[1][0] + ', M22 = ' + m.elements[1][1] + ')');
 				}
-
-				rule.addOldMsFilter('progid:DXImageTransform.Microsoft.Matrix(sizingMethod="auto expand", M11 = ' + m.elements[0][0] + ', M12 = ' + m.elements[0][1] + ', M21 = ' + m.elements[1][0] + ', M22 = ' + m.elements[1][1] + ')');
+				
 			}
-			
 		}
-	}
+	});
 };
 
 function toRadian (value) {
